67ae36a23417f8db7ccff74fa85155188cdb1b8c
hoohoo-b <teo.michelle.wt@gmail.com>
2017-09-28 00:20:10 +0800

    ADDITION OF BDENSE DATA
    BIUTIFUL!!! Praise Jesus :'))

--- Sample Dataset & Code for Clustering/clean_DENSE.py
+++ Sample Dataset & Code for Clustering/clean_DENSE.py
@@ -7,11 +7,12 @@ Created on Sat Sep 23 12:22:26 2017
 """
 
 import pandas as pd
-import numpy as np
 
 left = pd.read_csv("DENSE.csv", header=0)
-right = pd.read_csv("data_allSports.csv", header=0)
+left["index1"] = left["index1"].apply(lambda x: x+1)
+left["index2"] = left["index2"].apply(lambda x: x+1)
+right = pd.read_csv("probNew_AllSports.csv", header=0)
 
 result = pd.merge(left, right, how='left', on=['index1', 'index2'])
 
-result.to_csv('data_allSports_DENSE.csv', index=False)
+result.to_csv("data_allSports_DENSE.csv", index=False)
